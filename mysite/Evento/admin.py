f1e1f6607145620f8ffe62b5d6e812239b48ecd2
Igor Brito <igorbrytto@gmail.com>
2017-09-11 21:56:15 -0300

    second commit

--- mysite/Evento/admin.py
+++ mysite/Evento/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from Evento.models import Evento, EventoCientifico, Pessoa, PessoaFisica, PessoaJuridica, Autor, ArtigoCientifico, Inscricoes, TipoInscricao, ArtigoAutor
 
-# Register your models here.
+
+admin.site.register(Evento)
+admin.site.register(EventoCientifico)
+admin.site.register(Pessoa)
+admin.site.register(PessoaFisica)
+admin.site.register(PessoaJuridica)
+admin.site.register(Autor)
+admin.site.register(ArtigoCientifico)
+admin.site.register(Inscricoes)
+admin.site.register(TipoInscricao)
+admin.site.register(ArtigoAutor)
--- mysite/Evento/views.py
+++ mysite/Evento/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Evento, EventoCientifico, Pessoa, PessoaFisica, PessoaJuridica, Autor, ArtigoCientifico, ArtigoAutor, Inscricoes, TipoInscricao
 
-# Create your views here.
+
+def listaEvento(request):
+    retorno = "<h1>Eventos</h1>"
+    lista = Evento.objects.all()
+    for evento in lista:
+        retorno += '</br>Nome do Evento: {}</br>.format(evento.nome)'
+    return HttpResponse(retorno)
